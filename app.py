f50e3b0697b59e1eac525aae063367775e6d4cf0
Роман Чукаев <roman.chukaev1@gmail.com>
2024-11-23 14:56:51 +0700

    заготовка для ошибок

--- app.py
+++ app.py
@@ -1,7 +1,57 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def start():
     return 'Начало положено'
+
+
+
+# @app.errorhandler(404)
+# def not_found_404(err):
+#     return '''
+# <!doctype html>
+# <html>
+#     <head>
+#         <title>НГТУ, ФБ, Лабораторные работы</title>
+#         <link rel="stylesheet" href="''' + url_for('static', filename='styles.css') + '''">
+#     </head>
+#     <body>
+#         <header>
+#             НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+#         </header>
+
+#         <h2>Ошибка 404 - такой страницы не существует</h2>
+
+#         <footer>
+#             &copy; Чукаев Роман, ФБИ-24, 3 курс, 2024
+#         </footer>
+#     </body>
+# </html>
+# '''
+
+
+# @app.errorhandler(500)
+# def not_found_500(err):
+#     return '''
+# <!doctype html>
+# <html>
+#     <head>
+#         <title>НГТУ, ФБ, Лабораторные работы</title>
+#         <link rel="stylesheet" href="''' + url_for('static', filename='styles.css') + '''">
+#     </head>
+#     <body>
+#         <header>
+#             НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+#         </header>
+
+#         <h2>Ошибка 500 - сервер не смог обработать запрос</h2>
+#         <p>Подробности ошибки: ''' + str(err) + '''</p> 
+
+#         <footer>
+#             &copy; Чукаев Роман, ФБИ-24, 3 курс, 2024
+#         </footer>
+#     </body>
+# </html>
+# ''', 500
